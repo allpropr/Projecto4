45669321522c203ace1cce6432dde1c8bfcc1ae7
gabri <gabri@MSI>
2025-02-10 20:27:13 -0500

    add 2 variables

--- src/demo1/Demotest1.java
+++ src/demo1/Demotest1.java
@@ -2,4 +2,18 @@ package demo1;
 
 public class Demotest1 {
 
+	public static void main(String [] args) {
+		
+		System.out.println("Hello World");
+		
+		int a = 5;
+		int b = 24;
+		
+		int result = a + b;
+		
+		System.out.println(result);
+		
+	}
+	
+	
 }
